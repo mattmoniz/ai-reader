b5d5f1e6a78493002d80abbc5f1b6b1312c1b9ce
Matthew Moniz <Matt@Fastforward.sh>
2023-10-30 19:41:49 -0400

    fixed tailwind css issue. Added component scaffolding and finihed hero banner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import {logo} from '../assets'
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/mattmoniz/ai-reader")}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden"/>
+        <span className="orange_gradient">OpenAI GPT-4</span>
+        <h2 className="desc">Summarize long articles into more concise articles.</h2>
+      </h1>
+    </header>
+  );
+}
+
+export default Hero
