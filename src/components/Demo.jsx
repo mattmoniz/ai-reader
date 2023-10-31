1ab3f3ea495ea44c20fcc86aa6880b8c97a34ff4
Matthew Moniz <Matt@Fastforward.sh>
2023-10-31 14:44:32 -0400

    implemented react redux

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,31 +1,56 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const [allArticles, setAllArticles] = useState([])
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+    }
+    setArticle(newArticle);
+    console.log(newArticle);
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
-            alt="link_icon"
+            alt="link-icon"
             className="absolute left-0 my-2 ml-3 w-5"
           />
 
-          <input type="url"
-          placeholder="Enter a URL"
-          value=""
-          onChange={() => {}}
-          required
-          className="url_input peer"
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
           />
 
-          <button type="button"
-          className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"></button>
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            <p>↵</p>
+          </button>
         </form>
 
         {/* Browse URL history */}
